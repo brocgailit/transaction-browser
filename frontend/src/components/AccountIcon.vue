8190dc576b9eebd0ed5e9501bfc557f9c78de8e0
broc <brocgailit@gmail.com>
2022-10-05 15:03:11 +0300

    [frontend] feat(account-icon): owner initials

---
+++ frontend/src/components/AccountIcon.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+    
+const props = defineProps<{
+    owner: string
+}>()
+
+const initials = props.owner.split(/\s/).map(name => name.slice(0,1)).join('');
+
+</script>
+
+<template>
+    <div class="icon">
+        {{ initials }}
+    </div>
+</template>
+
+<style scoped>
+    .icon {
+        font-size: 1.5em;
+        width: 2.25em;
+        height: 2.25em;
+        background-color: var(--color-primary);
+        color: var(--color-white);
+        border-radius: 100%;
+    }
+</style>
