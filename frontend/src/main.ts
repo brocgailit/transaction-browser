908f12be68fd2134324324402570b795a361d33d
broc <brocgailit@gmail.com>
2022-10-05 13:59:30 +0300

    [frontend]: feat(icons): app icons
    add desired icons to main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,8 +4,30 @@ import router from './router'
 
 import './assets/main.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+    faCartShopping,
+    faRightLeft,
+    faSuitcase,
+    faDice,
+    faBolt,
+    faQuestion,
+    faChevronLeft,
+    faCircleNotch
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCartShopping);
+library.add(faRightLeft);
+library.add(faSuitcase);
+library.add(faDice);
+library.add(faBolt);
+library.add(faQuestion);
+library.add(faChevronLeft);
+library.add(faCircleNotch);
+
 const app = createApp(App)
 
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.use(router)
-
 app.mount('#app')
