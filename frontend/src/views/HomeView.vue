69917ee6177b5dd09769bc26dac8a0d2bc0a63a7
broc <brocgailit@gmail.com>
2022-10-05 15:09:36 +0300

    [frontend] feat(home): initial home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import { APIClient, type Account } from '@/client';
+import AccountList from '../components/AccountList.vue';
+
+const client = new APIClient();
+
+const accounts = await client.getAllAccounts();
+
 </script>
 
 <template>
   <main>
-    Home view
+    <h1>Select an account</h1>
+    <AccountList :accounts=accounts />
   </main>
 </template>
