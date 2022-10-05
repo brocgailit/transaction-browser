b889c1db7530d4bfc1dbcc7c30af9b89a6d42e4b
broc <brocgailit@gmail.com>
2022-10-05 15:04:55 +0300

    [frontend] feat(transactions-list): initial
    Adds initial transactions list unit test

---
+++ frontend/src/components/TransactionsList.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+import type { Transaction } from '../client';
+import TransactionListItem from './TransactionListItem.vue';
+
+defineProps<{
+    transactions: Transaction[]
+}>();
+
+</script>
+
+<template>
+    <div class="transactions-list">
+        <slot></slot>
+        <ul>
+            <TransactionListItem
+                v-for="transaction of transactions"
+                :key="transaction.id"
+                :transaction="transaction"
+            ></TransactionListItem>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    .transactions-list {
+        width: 100%;
+    }
+
+    .transactions-list, ul {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5em;
+    }
+    
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+</style>
